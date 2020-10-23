2808b0f7e6e46b9dcdf9356f6c86ab0a0fa988af
Shane Hofstetter <shane.hofstetter@gmail.com>
2020-10-23 17:02:10 +0200

    simplify pub/sub processing, add more logging

--- lib/activejob-google_cloud_pubsub/worker.rb
+++ lib/activejob-google_cloud_pubsub/worker.rb
@@ -14,36 +14,26 @@ module ActiveJob
       using PubsubExtension
 
       def initialize(queue: 'default', min_threads: 0, max_threads: Concurrent.processor_count, pubsub: Google::Cloud::Pubsub.new, logger: Logger.new($stdout))
-        @queue_name  = queue
+        @queue_name = queue
         @min_threads = min_threads
         @max_threads = max_threads
-        @pubsub      = pubsub
-        @logger      = logger
+        @pubsub = pubsub
+        @logger = logger
       end
 
       def run
         @logger&.info "Google Pub/Sub Worker running - min_threads: #{@min_threads.to_i}, max_threads: #{@max_threads.to_i}"
-        pool = Concurrent::ThreadPoolExecutor.new(min_threads: @min_threads, max_threads: @max_threads, max_queue: -1)
 
-        @pubsub.subscription_for(@queue_name).listen {|message|
+        subscriber = @pubsub.subscription_for(@queue_name).listen(streams: 1, threads: { callback: 1 }) do |message|
           @logger&.info "Message(#{message.message_id}) was received."
+          process message
+        end
 
-          begin
-            Concurrent::Promise.execute(args: message, executor: pool) {|msg|
-              process_or_delay msg
-            }.rescue {|e|
-              @logger&.error e
-            }
-          rescue Concurrent::RejectedExecutionError
-            Concurrent::Promise.execute(args: message) {|msg|
-              msg.modify_ack_deadline! 10.seconds.to_i
+        subscriber.on_error do |error|
+          @logger&.error(error)
+        end
 
-              @logger&.info "Message(#{msg.message_id}) was rescheduled after 10 seconds because the thread pool is full."
-            }.rescue {|e|
-              @logger&.error e
-            }
-          end
-        }.start
+        subscriber.start
 
         while true
           sleep 5
@@ -59,23 +49,11 @@ module ActiveJob
 
       private
 
-      def process_or_delay(message)
-        if message.time_to_process?
-          process message
-        else
-          deadline = [message.remaining_time_to_schedule, MAX_DEADLINE.to_i].min
-
-          message.modify_ack_deadline! deadline
-
-          @logger&.info "Message(#{message.message_id}) was scheduled at #{message.scheduled_at} so it was rescheduled after #{deadline} seconds."
-        end
-      end
-
       def process(message)
         timer_opts = {
           execution_interval: MAX_DEADLINE - 10.seconds,
-          timeout_interval:   5.seconds,
-          run_now:            true
+          timeout_interval: 5.seconds,
+          run_now: true
         }
 
         delay_timer = Concurrent::TimerTask.execute(timer_opts) {
@@ -84,24 +62,27 @@ module ActiveJob
 
         begin
           succeeded = false
-          failed    = false
+          failed = false
 
           ActiveJob::Base.execute JSON.parse(message.data)
 
           succeeded = true
-        rescue Exception
+        rescue Exception => e
           failed = true
 
-          raise
+          @logger&.error e
+          raise e
         ensure
           delay_timer.shutdown
 
           if succeeded || failed
+            @logger&.info("Message(#{message.message_id}) processing succeeded") if succeeded
+            @logger&.warn("Message(#{message.message_id}) processing failed") if failed
             message.acknowledge!
 
             @logger&.info "Message(#{message.message_id}) was acknowledged."
           else
-            # terminated from outside
+            @logger&.warn "terminated from outside"
             message.modify_ack_deadline! 0
           end
         end
